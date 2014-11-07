7492b97db1324ed714190e0a26efba6030623d1e
Mubashir Meddekar <mubashirmeddekar@yahoo.com>
2014-11-07 18:41:55 +0530

    TableViewController Sample in iOS 7
    Checkout the tableViewController along with the cell deleting
    functionality

--- TableTest/TableViewController.m
+++ TableTest/TableViewController.m
@@ -9,7 +9,9 @@
 #import "TableViewController.h"
 
 @interface TableViewController ()
-
+{
+    NSMutableArray *myArray;
+}
 @end
 
 @implementation TableViewController
@@ -18,6 +20,9 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    myArray = [[NSMutableArray alloc]initWithObjects:@"ManiKanan",@"Giridharan",@"Aravindh",@"Sanjay",@"Murugan",@"Pavithra",@"Mohammed",@"Mubashir",@"Meddekar",@"faisal",@"Nouman",@"Ganesh",@"More",@"Giriesh", nil];
+    
+    
 }
 
 - (void)didReceiveMemoryWarning
@@ -26,4 +31,41 @@
     // Dispose of any resources that can be recreated.
 }
 
+-(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return [myArray count];
+}
+
+-(NSInteger)numberOfSectionsInTableView:(UITableView *)tableView{
+    return 1;
+}
+
+-(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"cell"];
+    
+    cell.textLabel.text = [myArray objectAtIndex:indexPath.row];
+    
+    return cell;
+}
+
+-(void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    NSLog(@"The Row you selected is %@",[myArray objectAtIndex:indexPath.row]);
+}
+
+-(void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    if (editingStyle == UITableViewCellEditingStyleDelete) {
+        
+        [myArray removeObjectAtIndex:indexPath.row];
+        [tableView reloadData];
+        
+    }
+}
+
+-(UITableViewCellEditingStyle)tableView:(UITableView *)tableView editingStyleForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    return YES;
+}
 @end
